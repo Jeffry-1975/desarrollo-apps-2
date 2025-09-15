cbbfea76ec6be46131ac449da21c554f932f9c8d
Jefferson <jefferson-paredes@hotmail.com>
2025-09-15 01:43:44 -0500

    Tarea semana 1

---
+++ Semana 1/Programacion_Java/CEjemplo01/src/main/java/com/zonajava/mavenproject1/CEjemplo01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.zonajava.mavenproject1;
+
+import Vista.AplicaCuadrado;
+
+/**
+ *
+ * @author Jeffer
+ */
+public class CEjemplo01 {
+
+    public static void main(String[] args) {
+        AplicaCuadrado cuadrado = new AplicaCuadrado();
+        cuadrado.show();
+    }
+}
